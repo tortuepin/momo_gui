9f6e38f05fccf761ca345fa9b0486f4845acc919
tortuepin <kameyasu119@gmail.com>
2018-11-22 00:47:22 +0900

    スライドショーできるように

--- src/containers/SettingMenu.js
+++ src/containers/SettingMenu.js
@@ -5,10 +5,22 @@ export class SettingMenu extends Component {
   constructor(props) {
     super(props);
     this.back = this.back.bind(this)
+    this.play = this.play.bind(this)
+    this.stop = this.stop.bind(this)
+    this.state = {
+      playFlag : -1
+    }
   }
 
   play(){
     console.log("play")
+    this.setState({playFlag : 1})
+    this.props.startShow(1000)
+  }
+  stop(){
+    console.log("stop")
+    this.setState({playFlag : -1})
+    this.props.startShow(0)
   }
 
   back(){
@@ -17,11 +29,20 @@ export class SettingMenu extends Component {
   }
   
   render() {
-    console.log("uuu")
+    console.log(this.state.playFlag)
+    let p_str=""
+    let p_func
+    if(this.state.playFlag < 0){
+      p_str = "play"
+      p_func = this.play
+    }else{
+      p_str = "stop"
+      p_func = this.stop
+    }
     return (
       <div class="float-button">
         <p class="plus">+</p>
-        <p onClick={this.play} class="play">Play</p>
+        <p onClick={p_func} class="play">{p_str}</p>
         <p onClick={this.back} class="back">Back</p>
       </div>
     );
--- src/containers/slide.js
+++ src/containers/slide.js
@@ -6,9 +6,11 @@ export class Slider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      num : 0
+      num : 0,
+      play : 0
     }
     this.contents_num = this.props.contents.length
+    this.startShow = this.startShow.bind(this)
   }
 
   render_one(src) {
@@ -26,6 +28,20 @@ export class Slider extends Component {
     }
   }
 
+  play(time) {
+    return new Promise(resolve => {
+      setTimeout(resolve, time)
+    })
+    .then(() => {
+      if(this.state.play > 0){
+        this.next_image()
+      }
+    })
+  }
+
+  startShow(time){
+    this.setState({play : time})
+  }
   onKeyDown(e) {
     if (e.key === "ArrowLeft") {
       this.prev_image()
@@ -42,13 +58,20 @@ export class Slider extends Component {
     this.next_image();
   }
 
+  componentDidMount() {
+  }
+
   render() {
+    if(this.state.play > 0){
+      this.play(this.state.play)
+    }
     return (
       <div>
         <div onClick={(e) => this.onClick(e)} onKeyDown={(e) => this.onKeyDown(e)} tabIndex="-1">
           {this.render_one(this.props.contents[this.state.num])}
         </div>
-        <SettingMenu changePage={(u) => { this.props.changePage(u) }}/>
+        <SettingMenu changePage={(u) => { this.props.changePage(u) }}
+                     startShow={(t) => { this.startShow(t) }}/>
       </div>
     );
   }

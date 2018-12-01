3878284706610e72a74e486c0578a4a5f66d0d95
tortuepin <kameyasu119@gmail.com>
2018-12-01 16:28:35 +0900

    スペースキーでスライドショーの起動

--- src/containers/SettingMenu.js
+++ src/containers/SettingMenu.js
@@ -8,16 +8,12 @@ export class SettingMenu extends Component {
     this.back = this.back.bind(this)
     this.play = this.play.bind(this)
     this.stop = this.stop.bind(this)
-    this.state = {
-      playFlag : -1,
-      duration : 1000
-    }
   }
 
   play(){
     console.log("play")
     this.setState({playFlag : 1})
-    this.props.startShow(this.state.duration)
+    this.props.startShow(this.props.duration)
   }
   stop(){
     console.log("stop")
@@ -31,26 +27,22 @@ export class SettingMenu extends Component {
   }
   
   render() {
-    console.log(this.state.playFlag)
     let p_str=""
-    let p_func
-    if(this.state.playFlag < 0){
+    if(this.props.playFlag < 0){
       p_str = "play"
-      p_func = this.play
     }else{
       p_str = "stop"
-      p_func = this.stop
     }
     return (
       <div class="float-button">
         <p class="plus">+</p>
         <p>
           <InputSlider
-            duration={this.state.duration}
-            changeDuration={(x) => {this.setState({duration : x})}}
+            duration={this.props.duration}
+            changeDuration={(x) => {this.setDuration({duration : x})}}
           />
         </p>
-        <p onClick={p_func} class="play">{p_str}</p>
+        <p onClick={this.props.startShow} class="play">{p_str}</p>
         <p onClick={this.back} class="back">Back</p>
         <p> {this.props.num}/{this.props.contents_num}</p>
       </div>
--- src/containers/slide.js
+++ src/containers/slide.js
@@ -8,10 +8,17 @@ export class Slider extends Component {
     this.state = {
       num : 0,
       play : 0,
-      isPlay : 0
+      isPlay : 0,
+      duration : 1000,
+      playFlag : -1
     }
     this.contents_num = this.props.contents.length
     this.startShow = this.startShow.bind(this)
+    this.setDuration = this.setDuration.bind(this)
+  }
+
+  setDuration(time){
+    this.setState({duration:time})
   }
 
   render_one(src) {
@@ -43,14 +50,23 @@ export class Slider extends Component {
   }
 
   startShow(time){
-    this.setState({play : time})
+    if(this.state.playFlag < 0){
+      this.setState({play : time, playFlag : 1})
+    }else{
+      this.setState({play : 0, playFlag : -1, isPlay : 0})
+    }
   }
+
   onKeyDown(e) {
     if (e.key === "ArrowLeft") {
       this.prev_image()
     }
     else if (e.key === "Enter") {
       this.next_image()
+    }
+    else if (e.key=== " "){
+      console.log("space")
+      this.startShow(this.state.duration)
     }
     else{
       this.next_image()
@@ -65,19 +81,21 @@ export class Slider extends Component {
   }
 
   render() {
+    console.log(this.state)
     if(this.state.isPlay == 0 && this.state.play > 0){
       this.setState({isPlay : 1})
       this.play(this.state.play)
     }
     return (
-      <div>
         <div onClick={(e) => this.onClick(e)} onKeyDown={(e) => this.onKeyDown(e)} tabIndex="-1">
           {this.render_one(this.props.contents[this.state.num])}
-        </div>
-        <SettingMenu changePage={(u) => { this.props.changePage(u) }}
-                     startShow={(t) => { this.startShow(t) }}
-                     num={this.state.num + 1}
-                     contents_num={this.contents_num}/>
+          <SettingMenu changePage={(u) => { this.props.changePage(u) }}
+                       startShow={() => { this.startShow(this.state.duration) }}
+                       num={this.state.num + 1}
+                       contents_num={this.contents_num}
+                       duration={this.state.duration}
+                       setDuration={this.setDuration}
+                       playFlag={this.state.playFlag}/>
       </div>
     );
   }
